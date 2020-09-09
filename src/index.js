cdba1d7fd4b4888423948074369d4823771da7f6
luigi-moretti <66831232+luigi-moretti@users.noreply.github.com>
2020-09-09 16:36:30 -0300

    Início configuração das rotas

--- src/index.js
+++ src/index.js
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin'
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/admin" component={Admin}/>
+            </Switch>
+        </App>
+    </Router>
+),
   document.getElementById('root')
 );
 
+/*
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
