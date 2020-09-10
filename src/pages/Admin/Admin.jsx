62e2a685f82631aeb34448b4780d02262009ccc2
luigi-moretti <66831232+luigi-moretti@users.noreply.github.com>
2020-09-10 16:41:15 -0300

    Editado layout Admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Header from "../../components/Header";
+import ListaLinks from '../../components/ListaLinks';
 
 const Admin = () => (
     <div className="title">
-        <h4>Admin</h4>
-        <p>Essa é minha página de configuração.</p>
+        <h3 className="text-center">Bem-Vindo</h3>
+        <Header/>
+        <Form className="mx-auto col-md-4 col-sm-10 mb-5">
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Título</Form.Label>
+                <Form.Control type="text" placeholder="Título do Link" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Link</Form.Label>
+                <Form.Control type="text" placeholder="Endereço URL" />
+            </Form.Group>
+
+            <Button variant="outline-secondary" type="submit" block>
+                Cadastrar
+            </Button>
+        </Form>
+        <ListaLinks/>
     </div>
 )
 
